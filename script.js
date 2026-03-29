542b2806261bcd35e4914db4dfbadbf1ee2d532b
nhan-bread <191059715+nhan-bread@users.noreply.github.com>
2026-03-29 15:11:12 -0700

    Create 3x3 game board

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+const gameBoard = (() => {
+    const myGameBoard = [];
+    const columns = 3;
+    const rows = 3;
+
+    for (i = 1; i <= rows; i++) {
+        const boardRow = [];
+        myGameBoard.push(boardRow);
+        for (j = 1; j <= columns; j++) {
+            boardRow.push(null);
+        }
+    }
+    
+    const getGameBoard = () => myGameBoard;
+
+    return { getGameBoard };
+})();
+
+
 function createPlayer(marker) {
     let score;
     const updateScore = () => {
