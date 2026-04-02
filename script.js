4a15f69d1c1789ea222664e3fefd9f786615541e
nhan-bread <191059715+nhan-bread@users.noreply.github.com>
2026-04-02 16:43:42 -0700

    Add method to take player's choice and place marker

--- script.js
+++ script.js
@@ -10,21 +10,39 @@ const gameBoard = (() => {
             boardRow.push(null);
         }
     }
-    
+
+    const placePlayerChoice = (player, row, column) => {
+        myGameBoard[row].splice(column, 1, player.marker);
+        return myGameBoard;
+    }
+
     const getGameBoard = () => myGameBoard;
 
-    return { getGameBoard };
+    return { placePlayerChoice, getGameBoard };
 })();
 
 
 function createPlayer(marker) {
-    let score;
+    let score = 0;
     const updateScore = () => {
         score++;
     }
     const getScore = () => score;
+
     return { marker, getScore, updateScore };
 }
 
 const playerOne = createPlayer("X");
 const playerTwo = createPlayer("O");
+
+console.log(`Player One's choice: ${gameBoard.placePlayerChoice(playerOne, 1, 1)}`);
+console.log(`Player Two's choice: ${gameBoard.placePlayerChoice(playerTwo, 1, 0)}`);
+console.log(`Player One's choice: ${gameBoard.placePlayerChoice(playerOne, 2, 0)}`);
+console.log(`Player Two's choice: ${gameBoard.placePlayerChoice(playerTwo, 1, 2)}`);
+console.log(`Player One's score: ${playerOne.getScore()}`);
+console.log(`Player Two's score: ${playerTwo.getScore()}`);
+console.log(`Player One's score +1: ${playerOne.updateScore()}`);
+console.log(`Player One's score +1 again: ${playerOne.updateScore()}`);
+console.log(`Player Two's score +1: ${playerTwo.updateScore()}`);
+console.log(`Player One's final score (should be 2): ${playerOne.getScore()}`);
+console.log(`Player Two's final score (should be 1): ${playerTwo.getScore()}`);
